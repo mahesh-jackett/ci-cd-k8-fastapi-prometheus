a708312dab473f2e6885b3d33dd2144bb63d96e8
mahesh-jackett <111279642+mahesh-jackett@users.noreply.github.com>
2023-05-19 17:35:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Allowed All traffic"}
+    return {"Hello": "Allowed All traffic to ec2-18-179-204-125.ap-northeast-1.compute.amazonaws.com"}
