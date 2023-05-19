c373faee915c38d74c01896e9d35e8c6bc1ca695
mahesh-jackett <111279642+mahesh-jackett@users.noreply.github.com>
2023-05-19 12:27:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,8 +6,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
