837904c4c52fb93442a06b720bdd74b6db6370e1
Mahesh Deshwal <m.deshwal93@gmail.com>
2023-05-22 01:42:49 +0530

    added support to connect with mongoDB

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+import os
 
 app = FastAPI()
+
+try:
+    client = MongoClient(f"mongodb://{os.getenv('MONGODB_USERNAME')}:{os.getenv('MONGODB_PASSWORD')}@{os.getenv('MONGODB_URL')}")
+    print("DB Connection successful")
+except Exception as e: print(e)
 
 @app.get("/")
 def read_root():
